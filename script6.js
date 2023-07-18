b3680495a282d6ebac3329f3d011b8660f0444ef
zahargnat <superzaharic@gmail.com>
2023-07-18 23:50:20 +0300

    121

---
+++ script6.js
@@ -0,0 +1,45 @@
+const pricePerItem = 3500;
+
+const orderedQuantity = 4;
+
+const totalPrice = pricePerItem * orderedQuantity;
+
+console.log(totalPrice);
+
+const x = 12;
+
+const v = 18;
+
+console.log("x > v:", x > v);
+
+const SAVED_PASSWORD = "werty123";
+
+const PASSWORD = "password";
+
+const isMatch = SAVED_PASSWORD == PASSWORD;
+
+console.log(isMatch);
+
+let a = 5;
+
+a = a + 2;
+
+console.log(a);
+
+let b = 10;
+
+b = b - 4;
+
+console.log(b);
+
+let c = 15;
+
+c = c * 3;
+
+console.log(c);
+
+let d = 20;
+
+d = d / 10;
+
+console.log(d);
