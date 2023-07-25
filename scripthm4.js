3dbf951b55a6efbf405a0e8e24f84599f79a3725
zahargnat <superzaharic@gmail.com>
2023-07-26 02:48:21 +0300

    123123

---
+++ scripthm4.js
@@ -0,0 +1,57 @@
+const k1 = "5";
+
+console.log(Number.parseInt(k1));
+
+const k2 = "12djd334";
+
+console.log(Number.parseInt(k2));
+
+const k3 = "12.45asdwe12";
+
+console.log(Number.parseInt(k3));
+
+const k4 = "qwqweeewq";
+
+console.log(Number.parseInt(k4));
+
+const k5 = "5";
+
+console.log(Number.parseFloat(k5));
+
+const k6 = "12djd334";
+
+console.log(Number.parseFloat(k6));
+
+const k7 = "12.45asdwe12";
+
+console.log(Number.parseFloat(k7));
+
+const k8 = "qwqweeewq";
+
+console.log(Number.parseFloat(k8));
+
+console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+
+console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+
+const random = Math.random() * (19 - 3) + 1;
+
+console.log(Number.parseInt(random));
+
+const number = "3";
+
+const d = Math.sqrt(number);
+
+console.log(d);
+
+const a = 4;
+
+const b = "4";
+
+const c = parseInt(b);
+
+console.log(c);
+
+const f = toString(a);
+
+console.log(f);
